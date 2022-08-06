d6afff1e2eadeff206c159a0cd975e6f083184a7
dev <dev@localhost>
2022-08-06 16:13:42 +0000

    Add failed session removal

--- internal/server/session_handler.go
+++ internal/server/session_handler.go
@@ -29,8 +29,9 @@ func GetSessionHandler() *SessionHandler {
 
 func (s *SessionHandler) BroadcastNewJobs() {
 	log.Info().Msg("broadcasting new jobs")
-	s.RLock()
-	defer s.RUnlock()
+	badSessions := []*StratumSession{}
+	s.Lock()
+	defer s.Unlock()
 	var wg sync.WaitGroup
 	wg.Add(len(s.Sessions))
 	for session := range s.Sessions {
@@ -39,11 +40,15 @@ func (s *SessionHandler) BroadcastNewJobs() {
 			err := sess.triggerNewJob()
 			if err != nil {
 				log.Error().Err(err).Msgf("Could not push msg to session %s", sess.ip)
+				badSessions = append(badSessions, sess)
 			}
 			wg.Done()
 		}(session)
 	}
 	wg.Wait()
+	for _, session := range badSessions {
+		s.removeSession(session)
+	}
 }
 
 func (s *SessionHandler) removeSession(session *StratumSession) {
